1934ef26dc188fd444626b2e1d6701b356f7f8c5
MarvRoldan <marvinroldan_@hotmail.com>
2020-06-04 13:47:52 -0600

    Added search by category

---
+++ Booze-pedia/Booze-pedia/Models/BoozeCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booze_pedia.Models
+{
+    public class BoozeCategoryViewModel
+    {
+        public List<Booze> Boozes { get; set; }
+        public SelectList Category { get; set; }
+        public string BoozeCategory { get; set; }
+        public string SearchString { get; set; }
+    }
+}
