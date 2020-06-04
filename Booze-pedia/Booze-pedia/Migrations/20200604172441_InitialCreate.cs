1fc96b7d7ad97da7293be42b4eecaf2e2226e7dd
MarvRoldan <marvinroldan_@hotmail.com>
2020-06-04 11:50:56 -0600

    Finished seeddata

---
+++ Booze-pedia/Booze-pedia/Migrations/20200604172441_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Booze_pedia.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Booze",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Category = table.Column<string>(maxLength: 20, nullable: false),
+                    Description = table.Column<string>(maxLength: 250, nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    InStock = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Booze", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Booze");
+        }
+    }
+}
