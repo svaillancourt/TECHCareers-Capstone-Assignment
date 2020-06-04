81e574c78941232365ed277e112a24b52d8b2269
MarvRoldan <marvinroldan_@hotmail.com>
2020-06-04 11:16:31 -0600

    Added booze model, and data context

---
+++ Booze-pedia/Booze-pedia/Data/BoozeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Booze_pedia.Models;
+
+namespace Booze_pedia.Data
+{
+    public class BoozeContext :DbContext
+    {
+        public BoozeContext(DbContextOptions<BoozeContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Booze> Booze { get; set; }
+    }
+}
